6a6b55c765db40982eb02a4908f8e0f7aea60ee6
mayookhm <79088427+mayookhm@users.noreply.github.com>
2022-02-12 17:58:07 +0530

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 //import { StackNavigator } from '@react-navigation/stack';
 //import { createStackNavigator } from '@react-navigation/stack';
 //import { createStackNavigation } from '@react-navigation/stack';
-//import { createStack } from '@react-navigation/stack';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import HomeScreen from "./screens/Home";
 import StartMapScreen from "./screens/StarMap";
@@ -20,10 +20,11 @@ function App() {
         headerShown: false
       }}>
     
+     
         {/* <Stack.Screen component="Home" name={HomeScreen} /> */}
-        {/* <Stack.Screen name="Home" component={HomeScreen} /> */}
         {/* <Stack.Screen name="Home" component="HomeScreen" /> */}
         {/* <Stack.Screen name:"Home" component:{HomeScreen} /> */}
+            <Stack.Screen name="Home" component={HomeScreen} />
     
         <Stack.Screen name="StarMap" component={StartMapScreen} />
         <Stack.Screen name="DailyPic" component={DailyPicScreen} />
